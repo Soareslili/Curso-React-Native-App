a655416a84725359d99797b26ce8d33e9f252ccd
Soareslili <lidianesantossoares2@gmail.com>
2026-04-09 15:49:57 -0300

    Criando novas pastas do projeto, com Hemo, Menu

---
+++ src/components/Menu/Index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { styles } from '../Menu/Styles';
+
+export function Menu() {
+  return (
+    <View style={styles.container}>
+        <Text>Menu</Text>
+    </View>
+  );
+}
